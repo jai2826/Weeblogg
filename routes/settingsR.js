6af566c6be47377bfb01a7d1cca9a9ba6c31d78c
Jai Lakhmani <jailakhmani12345@gmail.com>
2021-11-10 20:59:12 +0530

    Updating adminpanel

--- routes/settingsR.js
+++ routes/settingsR.js
@@ -14,43 +14,6 @@ router.get("/admin/settings", async (req,res)=>{
         res.status(500).json(err)
     }
 })
-// router.get("/admin/post/addpost", async (req,res)=>{
-// try {
-//         res.render("../views/Dashboard/adminpanel.pug", {
-//             page: "addpost"
-//         })
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
 
-// router.post("/admin/post/addpost", async (req, res) => {
-//     try 
-//     {   
-//         const blog =  Post({
-//             title: req.body.title,
-//             img : req.body.img,
-//             content : req.body.content,
-//             author : req.body.author,
-//             category : req.body.category
-//         })
-//         await blog.save();
-//         res.render("../views/Dashboard/adminpanel.pug", {
-//             page: "post"
-//         })
-//     } catch (err) {
-//         res.render("Data not Saved")    
-//     }
-    
-// })
-
-// router.get("/admin/post/show", async (req,res)=>{
-    
-//     try {
-//         res.json(db.posts.find())
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
 
 module.exports = router
