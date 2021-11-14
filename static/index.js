0709fb56edd9386a896f5ace9959d8ca547c101a
Jai Lakhmani <jailakhmani12345@gmail.com>
2021-11-14 19:29:29 +0530

    updating color schemes

--- static/index.js
+++ static/index.js
@@ -6,3 +6,13 @@ hamburger.addEventListener('click', () => {
     navbar.classList.toggle('h-nav'),
     navmenu.classList.toggle('v-class');
 })
+
+function onChange() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=confirmpassword]');
+    if (confirm.value === password.value) {
+      confirm.setCustomValidity('');
+    } else {
+      confirm.setCustomValidity('Passwords do not match!!');
+    }
+  }
